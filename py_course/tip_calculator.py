d363c52816a39ca8b75996ec975a847ecd8173f9
TheSh4rk3y <thesh4rk3y@gmail.com>
2022-11-27 08:30:20 -0600

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   py_course/tip_calculator.py

--- py_course/tip_calculator.py
+++ py_course/tip_calculator.py
@@ -6,19 +6,13 @@
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 #Write your code below this line 👇1
-# cost of bill / 5 * 1.12
 total_ = input("How much was the  bill? ")
-#tc -> float
 total_int = float(total_)
 tip_ = input("What percentage would you like to tip? 10, 12, or 15? ")
-# tc -> float
 tip_int = float(tip_)
 tip_calc = tip_int / 100
-# There has to be a better way, but i improvised here.
 tip_calc += 1 
 people_ = input("How many Party Members to split the check? ")
-# tc -> int
 people_int = int(people_)
-# formula = total / people * tip .2f
 math_ = (total_int / people_int) * tip_calc  
 print(f"Each party member should pay: ${math_:.2f}")
