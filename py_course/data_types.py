1e024d8434d5a6ae90139cc9397fae71b92ef71b
TheSh4rk3y <thesh4rk3y@gmail.com>
2022-11-27 08:56:06 -0600

    Create data_types.py

---
+++ py_course/data_types.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+a_ = int(two_digit_number[0])
+b_ = int(two_digit_number[1])
+two_digit_number = a_ + b_
+print(two_digit_number)
+# print(two_digit_number[0] + " + " + two_digit_number[1] + " = " + str(int(two_digit_number[0]) + int(two_digit_number[1])))
+# I had trouble getting the format of the final print line so i had to add another. tried using \n couldn't quite get it right.
