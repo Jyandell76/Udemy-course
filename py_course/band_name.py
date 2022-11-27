accfbe172e044b29da37bccfb78e002668e10824
TheSh4rk3y <thesh4rk3y@gmail.com>
2022-11-27 09:00:54 -0600

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   py_course/band_name.py
    	new file:   py_course/bmi_calculator
    	new file:   py_course/life_in_weeks.py

---
+++ py_course/band_name.py
@@ -0,0 +1,11 @@
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+#Solution #1
+print("Hello there fellow Python user!")
+a = input('What city did you grow up in? \n')
+b = input("What is your pet's name? \n")
+print( "Your very own band name is: "+ a + ' ' + b )
+#Worked
+
+# edited -- added 2 lines in this to reflect the \n requirements.
+# edited -- added " band name" format.
+# lesson -- i need to pay closer attention. 2 oversights in 1 lesson.
---
+++ py_course/life_in_weeks.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age_ = int(age)
+age_l = 90 - age_
+age_d = age_l * 365
+age_w =  age_l * 52
+age_m = age_l * 12
+print(f"You have {age_d} days, {age_w } weeks, and {age_m } months left.")
+# You have x days, y weeks, and z months left.
+
+
+
