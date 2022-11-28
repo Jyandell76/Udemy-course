fbfd8c835ef25cb7a382ae338728b6c519b64fec
TheSh4rk3y <thesh4rk3y@gmail.com>
2022-11-27 23:12:32 -0600

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   py_course/odd_or_even.py

---
+++ py_course/odd_or_even.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this line 👇
+math_ = number % 2
+if math_ != 0:
+    print(f"This is an odd number.")
+else :
+    print(f"This is an even number.")
+
+
+
+
+
