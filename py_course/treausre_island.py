022bac40200ce31c7b34bc15d09fefeee5cdf7d2
TheSh4rk3y <thesh4rk3y@gmail.com>
2022-12-02 19:45:28 -0600

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   py_course/treasure_island.txt
    	new file:   py_course/treausre_island.py

---
+++ py_course/treausre_island.py
@@ -0,0 +1,142 @@
+import asyncio
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+async def display():
+    await asyncio.sleep(5)
+    print("Mentor: Dont be alarmed i'm just a friendly Marauding Pirate Mentor!!")
+asyncio.run(display())
+print(" ")
+print(" ")
+async def display():
+    await asyncio.sleep(5)
+    print("Mentor: Welcome to my island arrr..arrr...arrr....")
+asyncio.run(display())
+print(" ")
+print(" ")
+async def display():
+    await asyncio.sleep(5)
+    print("Mentor: If you can stay alive long enough, you too can becoma Pirate Mentor!!!...IF!!")
+asyncio.run(display())
+print(" ")
+async def display():
+    await asyncio.sleep(6)
+    print("Mentor: Now then... tell us who you be??...'n be quick about it...grrr")
+asyncio.run(display())
+n_user = input("Mentor: Please tell us who you arrrrrrr?! \n ")
+print(" ")
+print(" ")
+async def display():
+    await asyncio.sleep(5)
+    print(f"Mentor: ARRR... of course ... of course .. Baiter first class {n_user}!!! I knowed it was you all along!!")
+asyncio.run(display())
+async def display():
+    await asyncio.sleep(3)
+    print("Mentor: course... i did. course i did....grrrr")
+asyncio.run(display())
+async def display():
+    await asyncio.sleep(5)
+    print(f"Mentor: ok, ok, {n_user} heres the catch. you see...")
+asyncio.run(display())
+async def display():
+    await asyncio.sleep(5)
+    print(f"Mentor: There only be one way off this here island... but i don't be thinking you have it in you {n_user}")
+asyncio.run(display())
+async def display():
+    await asyncio.sleep(4)
+    print("Mentor: Good luck to Ye... arrr arrr..")
+asyncio.run(display())
+async def display():
+    await asyncio.sleep(2)
+    print(f"Mentor: ohhh waiiitt... baiter {n_user} baiter {n_user} the moost important thinggggg.....")
+asyncio.run(display())
+async def display():
+    await asyncio.sleep(1)
+    print("you hear faintly as you begin to fall.....")
+asyncio.run(display())
+async def display():
+    await asyncio.sleep(10)
+    print("Cricket: 'meep cheep'...")
+asyncio.run(display())
+async def display():
+    await asyncio.sleep(5)
+    print("Cricket: 'meep cheep' 'meep cheep'... the crickets sing as you slowly regain consciousness")
+asyncio.run(display())
+async def display():
+    await asyncio.sleep(3)
+    print("Mentor: 'shouting from above' only one way out... good luck.")
+asyncio.run(display())
+print(" ")
+print(" ")
+async def display():
+    await asyncio.sleep(3)
+    print("*** You are now awake. You have a choice to make. Ahead of you, you see a long hallway")
+asyncio.run(display())
+print("you can see an intersection in the road. You can turn left or right here.")
+chk_1 = input("Which direction would you like to turn? Left or Right? \n")
+chk_1 = chk_1.lower()
+print(f"{chk_1}")
+if chk_1 == "right":
+    print(f"You've made the wrong choice {n_user}")
+    print("GAME OVER")
+    exit
+elif chk_1 != "left":
+    print(f'EERIE VOICE: "You really should have thought better of that"')
+    print("you hear a deafening sound emerge from behind you....")
+    async def display():
+        await asyncio.sleep(3)
+        print("SCREEEEEEEEEEEEEEEEEECCCCCCHHHHHIIIIINNNNGGGGGGG")
+    asyncio.run(display())
+    async def display():
+        await asyncio.sleep(3)
+        print("SCREEEEEEEEEEEEEEEEEECCCCCCHHHHHIIIIINNNNGGGGGGG")
+    asyncio.run(display())
+    async def display():
+        await asyncio.sleep(3)
+        print("****YOU ARE SUFFERING SEVERE HEAD TRAUMA........")
+    asyncio.run(display())
+    async def display():
+        await asyncio.sleep(3)
+        print("1000 Tilly clones alll surround you at the door ... waiting to peee.....")
+    asyncio.run(display())
+    async def display():
+        await asyncio.sleep(3)
+        print("SCREEEEEEEEEEEEEEEEEECCCCCCHHHHHIIIIINNNNGGGGGGG")
+    asyncio.run(display())
+    async def display():
+        await asyncio.sleep(3)
+        print("SCREEEEEEEEEEEEEEEEEECCCCCCHHHHHIIIIINNNNGGGGGGG")
+    async def display():
+        await asyncio.sleep(3)
+        print("You are Dead!")
+        exit
+    asyncio.run(display())
+
+
+
