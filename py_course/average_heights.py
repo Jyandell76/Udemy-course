b720c2d88733610aaeabf4284e65126afc47dc42
TheSh4rk3y <thesh4rk3y@gmail.com>
2022-12-09 17:56:50 -0600

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   py_course/average_.txt
    	new file:   py_course/average_heights.py

---
+++ py_course/average_heights.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+#Write your code below this row 👇
+c_ = 0
+f_ = 0
+sum_ = int(0)
+for s in student_heights:
+    sum_ = sum_+student_heights[f_]
+    f_ += 1
+avg_ = round(sum_ / f_)
+print(f"{avg_}")    
+
+
+
+
+
+
+
